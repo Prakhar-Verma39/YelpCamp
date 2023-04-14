20af92a34dc5c94768600b756750622260bda1f9
Prakhar-Verma39 <103757447+Prakhar-Verma39@users.noreply.github.com>
2023-04-15 03:29:49 +0530

    controller implemented

--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
 const passport = require('passport');
+const auth = require('../controllers/auth');
 
-router.get('/register', (req, res) => {
-    res.render('users/register')
-});
+router.get('/register', auth.renderRegister);
 
-router.post('/register', catchAsync(async (req, res, next) => {
-    try{
-        const { email, username, password} = req.body;
-        const user = new User({email, username});
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if(err) return next(err);
-            req.flash('success','Welcome to Yelp Camp!');
-            res.redirect('/campgrounds');
-        })
-    }catch(e){
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
-}));
+router.post('/register', catchAsync(auth.register));
 
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
+router.get('/login', auth.renderLogin);
 
-router.post('/login', passport.authenticate('local', {
-    failureFlash: true, failureRedirect:"/login",  keepSessionInfo: true, failureMessage: true
-}),(req, res) => {
-    req.flash('success', 'welcome back!');
-    const redirectUrl = req.session.returnTo || '/campgrounds';
-    delete req.session.returnTo;
-    res.redirect(redirectUrl);
-})
+router.post('/login', passport.authenticate('local', { 
+    failureFlash: true, 
+    failureRedirect:"/login",  
+    keepSessionInfo: true, 
+    failureMessage: true
+}), auth.login);
 
-router.get('/logout', (req, res, next) => {
-    req.logout(err => {
-        if(err) return next(err);
-        req.flash('success', "Goodbye!");
-        res.redirect('/campgrounds');
-    });
-}); 
+router.get('/logout', auth.logout); 
 
 module.exports = router;
